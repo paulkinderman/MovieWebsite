aa3cfc2d820f946953aeabd65ca89a869601204f
paulkinderman <paul.d.kinderman@gmail.com>
2018-03-11 18:58:14 -0400

    updated settings

--- MovieWebsite/urls.py
+++ MovieWebsite/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('website.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
